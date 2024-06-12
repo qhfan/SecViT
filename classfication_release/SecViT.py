b57a69ef5dce77e22a65f69a14e4de16e97f7c5a
qhfan <67534736+qhfan@users.noreply.github.com>
2024-06-12 12:29:57 +0800

    Update SecViT.py

--- classfication_release/SecViT.py
+++ classfication_release/SecViT.py
@@ -11,11 +11,82 @@ import torch.nn as nn
 from timm.models.layers import DropPath, trunc_normal_
 from timm.models.registry import register_model
 from timm.models.vision_transformer import _cfg
+from fvcore.nn import FlopCountAnalysis, flop_count_table
 import time
 from einops import rearrange, einsum
 from einops.layers.torch import Rearrange
 from typing import Tuple
 
+class SwishImplementation(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, i):
+        result = i * torch.sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_tensors[0]
+        sigmoid_i = torch.sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+class MemoryEfficientSwish(nn.Module):
+    def forward(self, x):
+        return SwishImplementation.apply(x)
+
+def rotate_every_two(x):
+    x1 = x[:, :, :, ::2]
+    x2 = x[:, :, :, 1::2]
+    x = torch.stack([-x2, x1], dim=-1)
+    return x.flatten(-2)
+
+def theta_shift(x, sin, cos):
+    return (x * cos) + (rotate_every_two(x) * sin)
+
+class RoPE(nn.Module):
+
+    def __init__(self, embed_dim, num_heads):
+        '''
+        recurrent_chunk_size: (clh clw)
+        num_chunks: (nch ncw)
+        clh * clw == cl
+        nch * ncw == nc
+
+        default: clh==clw, clh != clw is not implemented
+        '''
+        super().__init__()
+        angle = 1.0 / (10000 ** torch.linspace(0, 1, embed_dim // num_heads // 4))
+        angle = angle.unsqueeze(-1).repeat(1, 2).flatten()
+        self.register_buffer('angle', angle)
+
+    
+    def forward(self, slen: Tuple[int]):
+        '''
+        slen: (h, w)
+        h * w == l
+        recurrent is not implemented
+        '''
+        # index = torch.arange(slen[0]*slen[1]).to(self.angle)
+        index_h = torch.arange(slen[0]).to(self.angle)
+        index_w = torch.arange(slen[1]).to(self.angle)
+        # sin = torch.sin(index[:, None] * self.angle[None, :]) #(l d1)
+        # sin = sin.reshape(slen[0], slen[1], -1).transpose(0, 1) #(w h d1)
+        sin_h = torch.sin(index_h[:, None] * self.angle[None, :]) #(h d1//2)
+        sin_w = torch.sin(index_w[:, None] * self.angle[None, :]) #(w d1//2)
+        sin_h = sin_h.unsqueeze(1).repeat(1, slen[1], 1) #(h w d1//2)
+        sin_w = sin_w.unsqueeze(0).repeat(slen[0], 1, 1) #(h w d1//2)
+        sin = torch.cat([sin_h, sin_w], -1) #(h w d1)
+        # cos = torch.cos(index[:, None] * self.angle[None, :]) #(l d1)
+        # cos = cos.reshape(slen[0], slen[1], -1).transpose(0, 1) #(w h d1)
+        cos_h = torch.cos(index_h[:, None] * self.angle[None, :]) #(h d1//2)
+        cos_w = torch.cos(index_w[:, None] * self.angle[None, :]) #(w d1//2)
+        cos_h = cos_h.unsqueeze(1).repeat(1, slen[1], 1) #(h w d1//2)
+        cos_w = cos_w.unsqueeze(0).repeat(slen[0], 1, 1) #(h w d1//2)
+        cos = torch.cat([cos_h, cos_w], -1) #(h w d1)
+
+        retention_rel_pos = (sin.flatten(0, 1), cos.flatten(0, 1))
+
+        return retention_rel_pos
 
 class LayerNorm2d(nn.Module):
     def __init__(self, dim, eps=1e-6):
@@ -31,7 +102,7 @@ class LayerNorm2d(nn.Module):
         x = x.permute(0, 3, 1, 2).contiguous()
         return x
 
-class SCA(nn.Module):
+class KClusterAttn(nn.Module):
 
     def __init__(self, embed_dim, num_heads, num_clusters):
         super().__init__()
@@ -78,9 +149,11 @@ class SCA(nn.Module):
         restored_tensor = torch.gather(target, dim=-2, index=indices.sort(dim=-1).indices.unsqueeze(-1).expand_as(target))
         return restored_tensor
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, sin: torch.Tensor, cos: torch.Tensor):
         '''
         x: (b c h w)
+        sin: (h w) d
+        cos: (h w) d
         '''
         bsz, _, h, w = x.size()
         l = h * w
@@ -90,6 +163,8 @@ class SCA(nn.Module):
 
         q, k, v = rearrange(qkv, 'b (m n d) h w -> m b n (h w) d', m=3, n=self.num_heads)
         # q, k, v -> b n (h w) d
+        q = theta_shift(q, sin, cos)
+        k = theta_shift(k, sin, cos)
 
         k = k * self.scaling
 
@@ -123,7 +198,7 @@ class FeedForwardNetwork(nn.Module):
         activation_fn=F.gelu,
         dropout=0.0,
         activation_dropout=0.0,
-        subconv=False
+        subconv=True
         ):
         super().__init__()
         self.embed_dim = embed_dim
@@ -161,7 +236,7 @@ class Block(nn.Module):
         self.layerscale = layerscale
         self.embed_dim = embed_dim
         self.norm1 = LayerNorm2d(embed_dim, eps=1e-6)
-        self.attn = SCA(embed_dim, num_heads, num_cluster)
+        self.attn = KClusterAttn(embed_dim, num_heads, num_cluster)
         self.drop_path = DropPath(drop_path)
         self.norm2 = LayerNorm2d(embed_dim, eps=1e-6)
         self.ffn = FeedForwardNetwork(embed_dim, ffn_dim)
@@ -171,13 +246,13 @@ class Block(nn.Module):
             self.gamma_1 = nn.Parameter(layer_init_values * torch.ones(1, embed_dim, 1, 1),requires_grad=True)
             self.gamma_2 = nn.Parameter(layer_init_values * torch.ones(1, embed_dim, 1, 1),requires_grad=True)
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor, sin: torch.Tensor, cos: torch.Tensor):
         x = x + self.pos(x)
         if self.layerscale:
-            x = x + self.drop_path(self.gamma_1 * self.attn(self.norm1(x)))
+            x = x + self.drop_path(self.gamma_1 * self.attn(self.norm1(x), sin, cos))
             x = x + self.drop_path(self.gamma_2 * self.ffn(self.norm2(x)))
         else:
-            x = x + self.drop_path(self.attn(self.norm1(x)))
+            x = x + self.drop_path(self.attn(self.norm1(x), sin, cos))
             x = x + self.drop_path(self.ffn(self.norm2(x)))
         return x
     
@@ -232,6 +307,7 @@ class BasicLayer(nn.Module):
         super().__init__()
         self.embed_dim = embed_dim
         self.depth = depth
+        self.RoPE = RoPE(embed_dim, num_heads)
 
         # build blocks
         self.blocks = nn.ModuleList([
@@ -247,8 +323,9 @@ class BasicLayer(nn.Module):
 
     def forward(self, x: torch.Tensor):
         _, _, h, w = x.size()
+        sin, cos = self.RoPE((h, w))
         for blk in self.blocks:
-            x = blk(x)
+            x = blk(x, sin, cos)
         if self.downsample is not None:
             x = self.downsample(x)
         return x
@@ -291,12 +368,12 @@ class PatchEmbed(nn.Module):
         x = self.proj(x)#(b c h w)
         return x
     
-class SecViT(nn.Module):
+class KClusterFormerSimpleNew(nn.Module):
 
     def __init__(self, in_chans=3, num_classes=1000, 
                  embed_dims=[96, 192, 384, 768], depths=[2, 2, 6, 2], num_heads=[3, 6, 12, 24], 
                  num_clusterss=[[64, 49], [16, 49], [4, 49], [1, 1]], mlp_ratios=[3, 3, 3, 3], drop_path_rate=0.1, 
-                 layerscales=[False, False, False, False], layer_init_values=1e-6):
+                 projection=1024, layerscales=[False, False, False, False], layer_init_values=1e-6):
         super().__init__()
 
         self.num_classes = num_classes
@@ -329,9 +406,11 @@ class SecViT(nn.Module):
             )
             self.layers.append(layer)
             
-        self.norm = nn.BatchNorm2d(self.num_features)
+        self.proj = nn.Conv2d(self.num_features, projection, 1)
+        self.norm = nn.BatchNorm2d(projection)
+        self.swish = MemoryEfficientSwish()
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.head = nn.Conv2d(self.num_features, num_classes, 1) if num_classes > 0 else nn.Identity()
+        self.head = nn.Conv2d(projection, num_classes, 1) if num_classes > 0 else nn.Identity()
 
         self.apply(self._init_weights)
 
@@ -361,8 +440,9 @@ class SecViT(nn.Module):
         for layer in self.layers:
             x = layer(x)
 
-
+        x = self.proj(x) #(b c h w)
         x = self.norm(x) #(b c h w)
+        x = self.swish(x)
 
         x = self.avgpool(x)  # B C 1 1
         return x
@@ -374,8 +454,8 @@ class SecViT(nn.Module):
         return x
 
 @register_model
-def SecViT_T(args):
-    model = SecViT(
+def KClusterFormerSimpleNew_T(args):
+    model = KClusterFormerSimpleNew(
         embed_dims=[64, 128, 256, 512],
         depths=[2, 2, 9, 2],
         num_heads=[2, 4, 8, 16],
@@ -388,8 +468,8 @@ def SecViT_T(args):
     return model
     
 @register_model
-def SecViT_S(args):
-    model = SecViT(
+def KClusterFormerSimpleNew_S(args):
+    model = KClusterFormerSimpleNew(
         embed_dims=[64, 128, 256, 512],
         depths=[3, 4, 18, 4],
         num_heads=[2, 4, 8, 16],
@@ -402,8 +482,8 @@ def SecViT_S(args):
     return model
 
 @register_model
-def SecViT_M(args):
-    model = SecViT(
+def KClusterFormerSimpleNew_M(args):
+    model = KClusterFormerSimpleNew(
         embed_dims=[80, 160, 320, 512],
         depths=[4, 8, 26, 8],
         num_heads=[5, 5, 10, 16],
@@ -416,8 +496,8 @@ def SecViT_M(args):
     return model
 
 @register_model
-def SecViT_L(args):
-    model = SecViT(
+def KClusterFormerSimpleNew_L(args):
+    model = KClusterFormerSimpleNew(
         embed_dims=[112, 224, 448, 640],
         depths=[4, 8, 26, 8],
         num_heads=[7, 7, 14, 20],
@@ -428,3 +508,4 @@ def SecViT_L(args):
     )
     model.default_cfg = _cfg()
     return model
+
